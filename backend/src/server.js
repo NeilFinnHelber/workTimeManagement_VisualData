a4dc008a9cfff7d603254d3d6f5e59e99dec0ca1
Pes220212 <167974238+Pes220212@users.noreply.github.com>
2026-03-12 11:27:54 +0100

    Import needed things and add app.listen in server.js (#74)
    * Basic Setup for server.js without pool
    
    part of:
    
    * Import needed things and add app.listen
    
    part of:

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`API listening on port ${port}`);
+});
